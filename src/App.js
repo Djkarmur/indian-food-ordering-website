0fde71ad0885eaced139ee1d575d8a5e81dbb017
Djkarmur <dipakkarmur45@gmail.com>
2022-04-02 23:48:56 +0530

    third

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Footer from "./components/Footer";
 import Menu from './pages/Menu';
 import About from './pages/About';
 import Contact from "./pages/Contact";
+import './App.css';
 
 const App = () =>{
   return(
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route path="/" exact element={<Home/>} />
-            <Route path="/Menu" exact element={<Menu/>} />
-            <Route path="/About" exact element={<About/>} />
-            <Route path="/Contact" exact element={<Contact/>} />
-        </Routes>
-        <Footer/>
-    </Router>
+    
+      <Router >
+          <Navbar />
+          <Routes>
+              <Route path="/" exact element={<Home/>} />
+              <Route path="/Menu" exact element={<Menu/>} />
+              <Route path="/About" exact element={<About/>} />
+              <Route path="/Contact" exact element={<Contact/>} />
+          </Routes>
+          <Footer/>
+      </Router>
+    
   )
 }
  export default App;
